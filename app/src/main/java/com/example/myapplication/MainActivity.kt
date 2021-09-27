d4f43ff58f9c71c1996ba71b08328716a0c5349b
Oliver <o.lyngholm@gmail.com>
2021-09-27 10:33:52 +0200

    First Iteration

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,5 +7,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val kulhydrater = Log("Kulhydrater")
+        kulhydrater.lowerBound = 0
+        val kalorier = Log("Kcal")
+        kulhydrater.lowerBound = 0
+        val ketoner = Log("Ketoner")
+        ketoner.lowerBound = 0
+
+        val logs = mutableListOf<Log>()
+        logs.add(kulhydrater)
+        logs.add(kalorier)
+        logs.add(ketoner)
+    }
+
+
+    class Log constructor(unit: String){
+        val values = mutableListOf<Int>()
+        var upperBound: Int? = null
+        var lowerBound: Int? = null
+        val unit = unit
     }
 }
