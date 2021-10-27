de4ab9df3ea42491816e7bae6800cc531686392c
Oliver <o.lyngholm@gmail.com>
2021-10-27 12:17:23 +0200

    Second interation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,20 +2,35 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.adapter.CustomAdapter
 import com.example.myapplication.controller.DataController
 import javax.sql.DataSource
 
 class MainActivity : AppCompatActivity() {
+    val dataController = DataController(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val dataController = DataController(this)
+        initData()
+        getMainPage()
+    }
+
+    fun initData() {
         dataController.initLogs()
         dataController.createLog("test1", "testunit1")
+        dataController.createLog("test2", "testunit2")
+    }
+
+    fun getMainPage() {
+        setContentView(R.layout.activity_main)
         val dataSet = dataController.loadLogs()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = CustomAdapter(dataSet)
+    }
+
+    fun clickedCreateLog(view: android.view.View) {
+        setContentView(R.layout.new_log)
+
     }
 }
