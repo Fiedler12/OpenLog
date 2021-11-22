13c5b3d510964187f09e9c9107dcc160adac578b
Fiedler12 <o.lyngholm@gmail.com>
2021-11-22 13:51:20 +0100

    Started work on single entry page and home page formatting

--- app/src/main/java/com/example/myapplication/Activities/RecyclerFragment.kt
+++ app/src/main/java/com/example/myapplication/Activities/RecyclerFragment.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication.Activities
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
@@ -9,15 +11,14 @@ import com.example.myapplication.adapter.CustomAdapter
 import com.example.myapplication.controller.DataController
 
 class RecyclerFragment : Fragment() {
-    public val dataController = DataController(this)
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    val dataController = DataController(this)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
         dataController.initLogs()
         dataController.createLog("test1", "testunit1")
         dataController.createLog("test2", "testunit2")
         val recyclerView = requireView().findViewById<RecyclerView>(R.id.recycler_view)
         val dataSet = dataController.loadLogs()
         recyclerView.adapter = CustomAdapter(dataSet)
-
     }
 }
